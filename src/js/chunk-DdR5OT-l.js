3c3c90216a180bf79ae61f49089543992ddb12b0
littleGo <1027782558@qq.com>
2026-01-16 14:01:48 +0800

    update src file from pakeplus

---
+++ src/js/chunk-DdR5OT-l.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./chunk-CvT-8fpa.js";import{K as o,o as s,J as t}from"./chunk-gpwU6gGF.js";const n=r({},[["render",function(r,n){const e=t("router-view");return s(),o(e)}]]);export{n as default};
